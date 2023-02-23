6c46295444dd64608137a5c453d3bbe9b8a29ec2
Ra Du <duisek.kyzylorda@gmail.com>
2023-02-23 15:13:06 -0400

    fetch data from Heroku API; implemented search bar

--- comps/Footer.js
+++ comps/Footer.js
@@ -6,10 +6,10 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer
-      class="flex items-center justify-center flex-col p-4 bg-white border w-full bottom-0 left-0 md:px-6 md:py-8 dark:bg-white-900"
+      className="flex items-center justify-center flex-col p-4 bg-white border w-full bottom-0 left-0 md:px-6 md:py-8 dark:bg-white-900"
     >
-      <div class="sm:flex sm:items-center sm:justify-between">
-        <Link href="/" class="flex items-center mb-4 sm:mb-0">
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <Link href="/" className="flex items-center mb-4 sm:mb-0">
           <Image
             alt="logo"
             src={logo}
@@ -22,10 +22,10 @@ const Footer = () => {
           />
         </Link>
       </div>
-      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © {" "}
-        <a href="https://flowbite.com/" class="hover:underline">
+        <a href="https://flowbite.com/" className="hover:underline">
           GenerDate™
         </a>
          . Plan your dates easier.
